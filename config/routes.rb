42813bbdeb6eaea32b74874c2d72f2326ff339ac
rabble <evan@protest.net>
2017-01-08 16:41:36 -0800

    fixing it so we have the default about site included in the rails app... to update regenerate from the jekyll and copy over html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :people
-  root to: redirect("/testpage.html")
+  #root to: "_site/index.html"
 
   resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
