9d0b75cb4d2d0a1824b8b18b5ea628519c810bc7
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-15 14:52:33 -0700

    Validation simplified & ultimately removed
    Phone number is the only one we really need.  Also, haven't figured out
    how to stop the AN form submission, so might as well just let it happen
    w/o a proper phone number.

--- public/indivisible/call.js
+++ public/indivisible/call.js
@@ -6,24 +6,14 @@ validatePhone = function(num) {
   return false;
 }
 
-// Thanks to https://stackoverflow.com/questions/160550/zip-code-us-postal-code-validation for ZIP validation regex
-validateZip = function(zip) {
-  console.log("Validating '" + zip + "' as ZIP");
-  if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zip)) return zip
-  return false
-}
 
-validateEmail = function() {
-  return !$(emailField).hasClass('error_input');
-}
 commanderData = function() {
   thisPhone = validatePhone($(phoneField).val());
-  thisLocation = validateZip($(zipField).val());
-  hasEmail = validateEmail();
+  thisLocation = $(zipField).val();
   console.log("Phone is " + thisPhone);
   console.log("ZIP is " + thisLocation);
-  console.log("Email is " +hasEmail);
-  if (thisPhone && thisLocation && hasEmail) {
+
+  if (thisPhone) {
 
     var data = {
       campaignId: '2',
@@ -35,16 +25,12 @@ commanderData = function() {
     return data;
 
   } else {
-    console.log("Found some data errors.");
-    error_msg = 'To connect you to your senator we need:';
-    if (!thisPhone) error_msg += '\n -your US phone number';
-    if (!thisLocation) error_msg += '\n -your ZIP code';
-    if (!hasEmail) error_msg += '\n -valid email address';
-    alert(error_msg);
-    return false
+    console.log('No phone number.');
+    //No use notifying the user, because I haven't figured out how to stop the AN form submission.
+    //alert('Please enter a valid US phone number.');
+
   }
 }
-
 
 
 makeTheCall = function() {
@@ -58,6 +44,7 @@ makeTheCall = function() {
       complete: function(res) {
         console.log("AJAX done");
         console.log(res);
+
       }
 
     });
