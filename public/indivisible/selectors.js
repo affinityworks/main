baf172347285785e254dfb202f412b774c34bf99
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-15 12:58:00 -0700

    Wait to set selectors until AN form loads

--- public/indivisible/selectors.js
+++ public/indivisible/selectors.js
@@ -1,2 +1,5 @@
-var phoneField = $('#phone');
-var zipField = $('#form-zip_code');
+$('#can_embed_form').load(function(){
+  console.log('Form loaded.');
+  var phoneField = $('#phone');
+  var zipField = $('#form-zip_code');
+});
