c718ee6267416994ea470cd5b3c633c4944b25d9
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-16 13:33:59 -0700

    Basic referrer controls
    Hide email field & opt in controls and enable setting default values
    for both.

--- public/indivisible/app/form.js
+++ public/indivisible/app/form.js
@@ -4,6 +4,8 @@ $(document).on('can_embed_loaded', function() {
   zipField = $('#form-zip_code');
   emailField = $('#form-email');
   theForm = $('form#new_answer');
+  optInChk = $("input#name_optin1");
+	optInCtls = $('li#d_sharing');
   $(theForm).submit(function(event) {
     makeTheCall();
   });
--- public/indivisible/app/referrer_controls.js
+++ public/indivisible/app/referrer_controls.js
@@ -1,39 +1,42 @@
-/*This script will provide methods for controlling which users see what, based on referrer codes. */
-
-optInChk = $("#name_optin1");
-optInCtls = $('li#d_sharing');
-modifyForm = function(referrer_settings) {
-  console.log(referrer_settings);
-  
+/*Methods to control which users see what, based on referrer codes. */
+modifyForm = function (referrer_settings, slug) {
+  //console.log(referrer_settings);
+  referrer_settings['hide opt in controls'] && $(optInCtls).hide();
+  referrer_settings['opt out'] && $(optInChk).prop('checked', false);
+  if (referrer_settings['hide email field']) {
+    $(emailField).hide();
+    $(emailField).val(referrer_settings['email field contents']);
+    if (referrer_settings['email field contents'] == 'user IP') {
+      retval = $.get('http://ipinfo.io', function (res) {
+        $(emailField).val(res.ip + '@' + slug + '.slug');
+      }, 'jsonp');
+    }
+  }
 }
-
-
-$(document).ready(function(){
-	try {
-		$.get('app/referrers.yaml',function(data){
-			if (data) {
-				referrer_data = jsyaml.load(data);
-				console.log(referrer_data);
-			
-				query_string = new URLSearchParams(window.location.search);
-				if(query_string.has('ref')){
-					var ref = query_string.get('ref');
-					console.log('Got ref slug ' + ref);
-					if(referrer_data.hasOwnProperty(ref)) {
-						referrer_settings = referrer_data[ref];
-						console.log('Found referrer ' + referrer_settings.fullname);
-						$(document).on('can_embed_loaded', modifyForm(referrer_settings));
-					}
-				} else {
-			
-				}
-			
-			} else {
-				console.log('Error while reading YAML referrer data.');
-			}
-		}, 'text');
-	
-	} catch (e) {
-		console.log(e);
-	}
+$(document).on('can_embed_loaded', function () {
+  try {
+    $.get('app/referrers.yaml', function (data) {
+      if (data) {
+        referrer_data = jsyaml.load(data);
+        //console.log(referrer_data);
+        query_string = new URLSearchParams(window.location.search);
+        if (query_string.has('ref')) {
+          var ref = query_string.get('ref');
+          console.log('Got ref slug ' + ref);
+          if (referrer_data.hasOwnProperty(ref)) {
+            referrer_settings = referrer_data[ref];
+          } else {
+            referrer_settings = referrer_data['default']
+          }
+          console.log('Using ' + referrer_settings.fullname + ' settings');
+          modifyForm(referrer_settings, ref);
+        } else {
+        }
+      } else {
+        console.log('Error while reading YAML referrer data.');
+      }
+    }, 'text');
+  } catch (e) {
+    console.log(e);
+  }
 });
