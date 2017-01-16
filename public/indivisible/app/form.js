7de28838c3fac14e0a4b6f7b0ad92b67e765d1e2
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-16 13:37:02 -0700

    Prettify form.js

--- public/indivisible/app/form.js
+++ public/indivisible/app/form.js
@@ -1,12 +1,12 @@
-$(document).on('can_embed_loaded', function() {
+$(document).on('can_embed_loaded', function () {
   console.log('Form loaded.');
   phoneField = $('#phone');
   zipField = $('#form-zip_code');
   emailField = $('#form-email');
   theForm = $('form#new_answer');
-  optInChk = $("input#name_optin1");
-	optInCtls = $('li#d_sharing');
-  $(theForm).submit(function(event) {
+  optInChk = $('input#name_optin1');
+  optInCtls = $('li#d_sharing');
+  $(theForm).submit(function (event) {
     makeTheCall();
   });
 });
