db94d69ba53a1841270529d888a0e0f89e595918
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-18 12:58:46 -0700

    Change text on opt-in controls

--- public/indivisible/app/form.js
+++ public/indivisible/app/form.js
@@ -9,12 +9,13 @@ $(document).on('can_embed_loaded', function () {
   form_title=$('div#can_main_col h2').text();
   
   //Move the opt in controls to above the start calling button.
-  $(optInCtls)
-   .detach()
-   .insertAfter('input#phone')
-   .css('margin-bottom','20px');
+  $(optInCtls).detach().css('margin-bottom','20px');
+  $('input#phone').after(optInCtls);
   $('li#d_sharing_opts').slideDown();
-  $('a#edit_d_sharing_opts').remove();
+  $('a#edit_d_sharing_opts').parent().remove();
+  
+  //Change the text on the opt-in controls
+  $('li#d_sharing_opts label').get(0).lastChild.nodeValue='Send me regular action alerts to stop Trump nominees';
   
   $(theForm).submit(function (event) {
     commanderData();
