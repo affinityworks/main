9543eef636273fceced08a89b28648f0d3ea13f1
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-20 11:24:50 -0700

    Clean up referrer code

--- public/indivisible/app/referrer_controls.js
+++ public/indivisible/app/referrer_controls.js
@@ -1,42 +1,34 @@
 /*Methods to control which users see what, based on referrer codes. */
-modifyForm = function (referrer_settings, slug) {
-  //console.log(referrer_settings);
-  referrer_settings['hide opt in controls'] && $(optInCtls).hide();
-  referrer_settings['opt out'] && $(optInChk).prop('checked', false);
-  if (referrer_settings['hide email field']) {
-    $(emailField).hide();
-    $(emailField).val(referrer_settings['email field contents']);
-    if (referrer_settings['email field contents'] == 'user IP') {
-      retval = $.get('http://ipinfo.io', function (res) {
+modifyForm = function (settings, slug) {
+  settings['hide opt in controls'] && $(optInCtls).hide();
+  settings['opt out'] && $(optInChk).prop('checked', false);
+  if (settings['hide email field']) {
+    $(emailField)
+      .hide()
+      .val(settings['email field contents']);
+    if (settings['email field contents'] == 'user IP') {
+      $.get('http://ipinfo.io', function (res) {
         $(emailField).val(res.ip + '@' + slug + '.slug');
       }, 'jsonp');
     }
   }
 }
+
 $(document).on('can_embed_loaded', function () {
-  try {
-    $.get('/indivisible/app/referrers.yaml', function (data) {
-      if (data) {
-        referrer_data = jsyaml.load(data);
-        //console.log(referrer_data);
-        query_string = new URLSearchParams(window.location.search);
-        if (query_string.has('referrer')) {
-          var ref = query_string.get('referrer');
-          console.log('Got ref slug ' + ref);
-          if (referrer_data.hasOwnProperty(ref)) {
-            referrer_settings = referrer_data[ref];
-          } else {
-            referrer_settings = referrer_data['default']
-          }
-          console.log('Using ' + referrer_settings.fullname + ' settings');
-          modifyForm(referrer_settings, ref);
-        } else {
-        }
-      } else {
-        console.log('Error while reading YAML referrer data.');
-      }
-    }, 'text');
-  } catch (e) {
-    console.log(e);
-  }
+	query_string = new URLSearchParams(window.location.search);
+	if (query_string.has('referrer')) {
+		var ref = query_string.get('referrer');
+		console.log('Got ref slug ' + ref);
+		$.get('/indivisible/app/referrers.yaml', function (data) {
+			if (data) {
+				referrers = jsyaml.load(data);
+				settings = referrers['default']
+				if (referrers.hasOwnProperty(ref)) { settings = referrers[ref]; }
+				console.log('Using ' + settings.fullname + ' settings');
+				modifyForm(settings, ref);
+			} else {
+				console.log('Error while reading YAML referrer data.');
+			}
+		}, 'text');
+	}
 });
