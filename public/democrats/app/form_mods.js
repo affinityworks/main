e6aaf36fb35b6d59f5608c03ff7c7b607f5bab89
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-21 17:46:25 -0700

    Split form.js into form.js and form_mods.js

---
+++ public/democrats/app/form_mods.js
@@ -0,0 +1,29 @@
+$(document).on('can_embed_loaded', function () {
+  console.log('Form loaded.');
+  phoneField = $('#phone');
+  zipField = $('#form-zip_code');
+  emailField = $('#form-email');
+  theForm = $('form#new_answer');
+  optInChk = $('input#name_optin1');
+  optInCtls = $('li#d_sharing');
+  form_title = $('div#can_main_col h2').text();
+  
+  //Move the opt in controls to above the start calling button.
+  $(optInCtls).detach().css('margin-bottom', '20px');
+  $('input#phone').after(optInCtls);
+  $('li#d_sharing_opts').slideDown();
+  $('a#edit_d_sharing_opts').parent().remove();
+  
+  //Change the text on the opt-in controls
+  $('li#d_sharing_opts label').get(0).lastChild.nodeValue = 'Send me regular action alerts to stop Trump nominees';
+  
+  //Move the disclaimer to the bottom
+  disclaimer = $('li#d_sharing ul li').first();
+  $(disclaimer).detach().addClass('new-disclaimer');
+  $('form#new_answer ul').first().append(disclaimer);
+});
+
+$(document).on('can_embed_submitted', function () {
+	//Remove the embed block.
+  $('div.clearfix div.can_thank_you-block').last().remove();
+});
