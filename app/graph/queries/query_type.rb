d57049f836082947a833bd905c776954ff7caaa4
rabble <evan@protest.net>
2017-01-22 18:05:54 -0500

    fixing typo bug

--- app/graph/queries/query_type.rb
+++ app/graph/queries/query_type.rb
@@ -9,17 +9,17 @@ QueryType = GraphQL::ObjectType.define do
 
   # You can define fields on the fly:
   field :hero do
-    type -> { !CharacterInterface }
+    #type -> { !CharacterInterface }
     description "The hero of the saga"
 
-    argument :episode, EpisodeEnum, "If provided, return the hero of that episode"
+    #argument :episode, EpisodeEnum, "If provided, return the hero of that episode"
 
     resolve -> (obj, args, ctx) do
-      args["episode"] == 5 ? Human.find(1000) : Droid.find(2001)
+      #args["episode"] == 5 ? Human.find(1000) : Droid.find(2001)
     end
   end
 
-  field :human, HumanType, field: FetchField.new(type: HumanType, model: Human)
-  field :droid, DroidType, field: FetchField.new(type: DroidType, model: Droid)
+  #field :human, HumanType, field: FetchField.new(type: HumanType, model: Human)
+  #field :droid, DroidType, field: FetchField.new(type: DroidType, model: Droid)
   field :node, GraphQL::Relay::Node.field
 end
