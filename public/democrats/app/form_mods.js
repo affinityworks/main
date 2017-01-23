6cb2a9f874d92a7a2eb9078093ccadad7e86f467
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-22 22:25:41 -0700

    Thank you page mods

--- public/democrats/app/form_mods.js
+++ public/democrats/app/form_mods.js
@@ -21,6 +21,20 @@ $(document).on('can_embed_loaded', function () {
 });
 
 $(document).on('can_embed_submitted', function () {
+  thank_you_div = $('div#can_thank_you');
 	//Remove the embed block.
   $('div.clearfix div.can_thank_you-block').last().remove();
+  
+  //Change the form title
+  $('div#can_thank_you h1').text('Thank you!');
+  
+  //Alter text of h4 and put after the thank you text
+  $('div#can_thank_you h4').text('Help us build a powerful rapid response network by spreading the word using the tools below.').detach().appendTo($(thank_you_div));
+  
+  //Create a label for the email sample textarea
+  new_label = $('<label>')
+    .addClass('block mt20 graytext')
+    .attr('id','email_label')
+    .text('Hightlight the whole message below, then copy and paste it into an email.')
+    .insertBefore('textarea#form-email_friend_message');
 });
