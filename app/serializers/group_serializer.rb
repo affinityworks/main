954dea446c1444a9d09eb4e9287aa5c49955692c
rabble <evan@protest.net>
2017-01-25 12:44:49 -0500

    resolving conflict

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,12 +1,7 @@
 class GroupSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :origin_system, :name, :description, :summary, :browser_url, :featured_image_url, :creator_id
-  has_many :advocacy_campaigns_groups
-=======
   attributes :origin_system, :name,  :description,  :summary,  :browser_url,  :featured_image_url
   belongs_to :creator, :class_name => "Person"
   belongs_to :modified_by, :class_name => "Person"
->>>>>>> d57049f836082947a833bd905c776954ff7caaa4
 end
 
 
