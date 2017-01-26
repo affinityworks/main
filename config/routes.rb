eed59fd3f2a1cbf5f4e2f67657122d1f543feb49
rabble <evan@protest.net>
2017-01-25 23:38:52 -0500

    adding zipcode service

--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,5 @@ Rails.application.routes.draw do
   #mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/queries"
   resources :queries
   resource :sha, only: :show
+  resources :zipcodes, only: :show
 end
---
+++ test/controllers/zipcodes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ZipcodesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
