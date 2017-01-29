9d3e8854775d2564f15e076de3f7763b236c651a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-29 11:15:39 -0700

    Add SMS terms & conditions

--- public/call/app/form.js
+++ public/call/app/form.js
@@ -8,6 +8,9 @@ $(document).on('can_embed_loaded', function () {
   optInCtls = $('li#d_sharing');
   form_title = $('div#can_main_col h2').text();
   head_container = $('div#heads');
+	$('a#terms_link').click(function(){
+		alert("Advocacy Commons SMS Terms & Conditions:\nBy signing up and entering my telephone number, I consent to receive and authorize Advocacy Commons to send (i) SMS text messages, (ii) prerecorded audio messages (including calls to cellular phones), or (iii) other communications sent by an autodialer to my mobile device.");
+	});
 });
 
 replaceThankYou = function (targetHTML) {
