e3a4c47f68c81f7de801d303256c244b807d924a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-29 11:24:44 -0700

    Slight changes to ref passthrough script

--- public/call/app/ref_passthrough.js
+++ public/call/app/ref_passthrough.js
@@ -1,10 +1,8 @@
 var base_embed_url = 'https://actionnetwork.org/widgets/v2/form/stop-rex-tillerson-2?format=js&source=widget&style=full'
 query_string = new URLSearchParams(window.location.search);
 var ref = query_string.get('referrer');
-
 var full_embed_url = base_embed_url + '&referrer='+ref;
-console.log(full_embed_url);
 $.getScript(full_embed_url)
  .done(function (script, textStatus) {
-   console.log(textStatus);
+   console.log('Loading AN embed script: ' + textStatus);
  });
