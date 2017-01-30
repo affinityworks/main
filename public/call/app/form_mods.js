cd26b70910f2309dbb001c7b875a5dfcef701f7a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-01-29 23:18:55 -0700

    Reenable /call and /signup

---
+++ public/call/app/form_mods.js
@@ -0,0 +1,44 @@
+//Modify the AN form to look the way we want it to.
+
+$(document).on('can_embed_loaded', function () {
+  
+  //Move the opt in controls to above the start calling button.
+  // $(optInCtls).detach().css('margin-bottom', '20px');
+//   $('input#phone').after(optInCtls);
+//   $('li#d_sharing_opts').slideDown();
+//   $('a#edit_d_sharing_opts').parent().remove();
+//   
+//   //Change the text on the opt-in controls
+//   $('li#d_sharing_opts label').get(0).lastChild.nodeValue = 'Send me regular action alerts to stop Trump nominees';
+//   
+//   //Move the disclaimer to the bottom
+//   disclaimer = $('li#d_sharing ul li').first();
+//   $(disclaimer)
+//     .detach()
+//     .addClass('new-disclaimer')
+//     .html('You will receive updates from <em>Democrats.com,</em> the sponsor of this form.  By signing up and entering your telephone number, you consent to receive text messages, prerecorded audio messages (including calls to cellular phones), and other communications.');
+//   $('form#new_answer ul').first().append(disclaimer);
+    
+  //Remove the "not in the US?" dropdown link
+  $('div.country_wrap').remove();
+});
+
+$(document).on('can_embed_submitted', function () {
+  thank_you_div = $('div#can_thank_you');
+	//Remove the embed block.
+  $('div.clearfix div.can_thank_you-block').last().remove();
+  
+  //Change the form title
+  $('div#can_thank_you h1').text('Thank you!');
+  
+  //Alter text of h4 and put after the thank you text
+  $('div#can_thank_you h4').html('Help us build a powerful rapid response network &mdash; spread the word using the tools below.').detach().appendTo($(thank_you_div));
+  
+  //Create a label for the email sample textarea
+  new_label = $('<label>')
+    .addClass('block mt20 graytext')
+    .attr('id','email_label')
+    .text('Hightlight the whole message below, then copy and paste it into an email.')
+    .insertBefore('textarea#form-email_friend_message');
+  
+});
---
+++ public/signup/app/form.js
@@ -0,0 +1,18 @@
+$(document).on('can_embed_loaded', function () {
+  console.log('Form loaded.');
+  phoneField = $('#phone');
+  zipField = $('#form-zip_code');
+  emailField = $('#form-email');
+  theForm = $('form#new_answer');
+  optInChk = $('input#name_optin1');
+  optInCtls = $('li#d_sharing');
+  form_title = $('div#can_main_col h2').text();
+  head_container = $('div#heads');
+	$('a#terms_link').click(function(){
+		alert("Advocacy Commons SMS Terms & Conditions:\nBy signing up and entering my telephone number, I consent to receive and authorize Advocacy Commons to send (i) SMS text messages, (ii) prerecorded audio messages (including calls to cellular phones), or (iii) other communications sent by an autodialer to my mobile device.");
+	});
+});
+
+$(document).on('can_embed_submitted', function() {
+  
+});
