3dc6c9c41a6a1699a7f1128bbcaf782bb27d8d58
Scott Willson <scott.willson@gmail.com>
2017-02-03 18:51:02 -0800

    Use single quotes

--- test/controllers/api/v1/entry_point_controller_test.rb
+++ test/controllers/api/v1/entry_point_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Api::V1::EntryPointControllerTest < ActionDispatch::IntegrationTest
-  test "#show" do
+  test '#show' do
     get api_v1_url as: :json
     assert_response :success
 
