b802ef340aa40a42cd263401a7ed8d29a42c6d50
Scott Willson <scott.willson@gmail.com>
2017-02-03 18:48:51 -0800

    Use OSDI names. Move root API views to entry_point.

--- test/controllers/api/v1/entry_point_controller_test.rb
+++ test/controllers/api/v1/entry_point_controller_test.rb
@@ -4,5 +4,8 @@ class Api::V1::EntryPointControllerTest < ActionDispatch::IntegrationTest
   test "#show" do
     get api_v1_url as: :json
     assert_response :success
+
+    json = JSON.parse(response.body)
+    assert json['_links'].present?
   end
 end
