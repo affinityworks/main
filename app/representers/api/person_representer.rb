befe8b6c5e08287c9697f146a0506c36b7d8e0f9
Scott Willson <scott.willson@gmail.com>
2017-02-08 12:21:00 -0800

    Add simple properties to PersonRepresenter

--- app/representers/api/person_representer.rb
+++ app/representers/api/person_representer.rb
@@ -4,12 +4,27 @@ class Api::PersonRepresenter < Roar::Decorator
   include Roar::JSON::HAL
 
   property :created_at, as: :created_date
-  property :ethnicities
   property :id
-  property :given_name
   property :updated_at, as: :modified_date
 
   property :identifiers, exec_context: :decorator
+
+  property :ethnicities
+  property :given_name
+
+  property :additional_name
+  property :birthdate
+  property :employer
+  property :ethnicities
+  property :family_name
+  property :gender
+  property :gender_identity
+  property :given_name
+  property :honorific_prefix
+  property :honorific_suffix
+  property :languages_spoken
+  property :party_identification
+  property :source
 
   def identifiers
     ["osdi:#{ActiveModel::Naming.singular(represented)}-#{represented.id}"]
