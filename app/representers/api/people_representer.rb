4524e725152cf772b5fe1eb70ae0b808aaebf147
Scott Willson <scott.willson@gmail.com>
2017-02-08 12:08:26 -0800

    Use Rails routing helpers in PeopleRepresenter

--- app/representers/api/people_representer.rb
+++ app/representers/api/people_representer.rb
@@ -6,20 +6,28 @@ class Api::PeopleRepresenter < Representable::Decorator
 
   link :self do
     api_v1_people_url
-    # '/api/v1/people'
   end
 
   collection :to_a, as: 'osdi:people', class: Person, extend: Api::PersonRepresenter, embedded: true
 
   link :previous do
-    "/api/v1/people?page=#{represented.prev_page}"
+    api_v1_people_path(page: represented.prev_page)
+  end
+
+  link :next do
+    api_v1_people_path(page: represented.next_page)
   end
 
   property :current_page, as: :page
   property :size, as: :total_pages
   property :total_count, as: :total_records
 
+  def initialize(people, request)
+    @request = request
+    super people
+  end
+
   def default_url_options
-    Rails.application.default_url_options or {}
+    { host: @request.host }
   end
 end
