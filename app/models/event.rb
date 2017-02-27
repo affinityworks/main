b3df4874ec8de4da9a9b46b0a82963f22d7724bf
Scott Willson <scott.willson@gmail.com>
2017-02-26 17:13:15 -0800

    Fix minor lint

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   has_many :ticket_levels
 
-  belongs_to :location, :class_name => "Address", :foreign_key => 'address_id'
-  belongs_to :creator, :class_name => "Person"
-  belongs_to :organizer, :class_name => "Person"
-  belongs_to :modified_by, :class_name => "Person"
+  belongs_to :location, class_name: 'Address', foreign_key: :address_id
+  belongs_to :creator, class_name: 'Person'
+  belongs_to :organizer, class_name: 'Person'
+  belongs_to :modified_by, class_name: 'Person'
   has_many :attendances
   has_many :reminders
 
