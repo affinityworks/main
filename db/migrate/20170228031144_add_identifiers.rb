f3478c4845703597639e6904fbd6f2c4afff4002
Scott Willson <scott.willson@gmail.com>
2017-03-04 07:50:18 -0800

    #13 Correctly add identifiers

--- db/migrate/20170228031144_add_identifiers.rb
+++ db/migrate/20170228031144_add_identifiers.rb
@@ -1,21 +1,72 @@
 class AddIdentifiers < ActiveRecord::Migration[5.0]
   def change
-    add_column :advocacy_campaigns, :identifiers, :text, array: true, default: []
-    add_column :answers, :identifiers, :text, array: true, default: []
-    add_column :canvasses, :identifiers, :text, array: true, default: []
-    add_column :canvassing_efforts, :identifiers, :text, array: true, default: []
-    add_column :donations, :identifiers, :text, array: true, default: []
-    add_column :events, :identifiers, :text, array: true, default: []
-    add_column :forms, :identifiers, :text, array: true, default: []
-    add_column :fundraising_pages, :identifiers, :text, array: true, default: []
-    add_column :outreaches, :identifiers, :text, array: true, default: []
-    add_column :petitions, :identifiers, :text, array: true, default: []
-    add_column :queries, :identifiers, :text, array: true, default: []
-    add_column :questions, :identifiers, :text, array: true, default: []
-    add_column :scripts, :identifiers, :text, array: true, default: []
-    add_column :script_questions, :identifiers, :text, array: true, default: []
-    add_column :share_pages, :identifiers, :text, array: true, default: []
-    add_column :signatures, :identifiers, :text, array: true, default: []
-    add_column :submissions, :identifiers, :text, array: true, default: []
+    # Needs to be change_table; add_column doesn't create correct SQL
+    change_table :advocacy_campaigns do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :answers do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :canvasses do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :canvassing_efforts do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :donations do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :events do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :forms do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :fundraising_pages do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :outreaches do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :petitions do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :queries do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :questions do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :scripts do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :script_questions do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :share_pages do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :signatures do |t|
+      t.text :identifiers, array: true, default: []
+    end
+
+    change_table :submissions do |t|
+      t.text :identifiers, array: true, default: []
+    end
   end
 end
