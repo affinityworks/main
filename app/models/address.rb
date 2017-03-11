13a4134807d71a022ded84fb92c791aeab682fed
Scott Willson <scott.willson@gmail.com>
2017-03-11 10:15:06 -0800

    Use Ruby 1.9 hash syntax

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,11 @@
 class Address < ApplicationRecord
   serialize :address_lines, Array
   belongs_to :person
-  acts_as_mappable :default_units => :miles,
-                 :default_formula => :sphere,
-                 :distance_field_name => :distance,
-                 :lat_column_name => :latitude,
-                 :lng_column_name => :longitude
+  acts_as_mappable default_units: :miles,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :latitude,
+                   lng_column_name: :longitude
 
   def location
     { latitude: latitude, longitude: longitude }
