3a2a8aeb8e61be0f858424ad6f3f483468003b49
Scott Willson <scott.willson@gmail.com>
2017-03-11 14:50:24 -0800

    Test that previously-imported Action Network people are updated

--- test/models/api/action_network/people_test.rb
+++ test/models/api/action_network/people_test.rb
@@ -2,6 +2,22 @@ require 'test_helper'
 
 class Api::ActionNetwork::PeopleTest < ActiveSupport::TestCase
   test '.import!' do
+    person_not_from_action_network = Person.create!(family_name: 'Flanders', updated_at: Time.zone.local(2016))
+
+    unchanged_person = Person.create!(
+      identifiers: ['action_network:d91b4b2e-ae0e-4cd3-9ed7-d0ec501b0bc3'],
+      given_name: 'John',
+      family_name: 'Smith',
+      updated_at: Time.utc(2014, 2, 20, 21, 16, 57)
+    )
+
+    person_with_updates = Person.create!(
+      identifiers: ['action_network:d32fcdd6-7366-466d-a3b8-7e0d87c3cd8b'],
+      given_name: 'Mayor',
+      family_name: 'Quimby',
+      updated_at: Time.utc(2012)
+    )
+
     stub_request(:get, 'https://actionnetwork.org/api/v2/people')
       .with(headers: { 'OSDI-API-TOKEN' => 'test-token' })
       .to_return(body: File.read(Rails.root.join('test', 'fixtures', 'files', 'people.json')))
@@ -10,9 +26,24 @@ class Api::ActionNetwork::PeopleTest < ActiveSupport::TestCase
       Api::ActionNetwork::People.import!
     end
 
-    # TODO: assert non-AN person unchanged
-    # TODO: assert new person added correctly
-    # TODO: assert unchanged person not updated
-    # TODO: assert updated person was updated
+    assert_equal Time.zone.local(2016), person_not_from_action_network.reload.updated_at
+
+    new_person = Person.any_identifier('action_network:1efc3644-af25-4253-90b8-a0baf12dbd1e').first!
+    assert_equal 'Jane', new_person.given_name
+    assert_equal 'Doe', new_person.family_name
+    assert_equal Time.utc(2014, 2, 20, 21, 16, 56), new_person.updated_at, 'Use Action Network updated time, not current time'
+
+    # Originally imported from AN and there are no updates: should not change
+    assert Person.any_identifier('action_network:d91b4b2e-ae0e-4cd3-9ed7-d0ec501b0bc3').exists
+    unchanged_person.reload
+    assert_equal Time.utc(2014, 2, 20, 21, 16, 57), unchanged_person.updated_at
+    assert_equal 'John', unchanged_person.given_name
+    assert_equal 'Smith', unchanged_person.family_name
+
+    # Originally imported from AN; should apply updates
+    person_with_updates.reload
+    assert_equal Time.utc(2014, 12, 31), person_with_updates.updated_at
+    assert_equal "Joseph Fitzgerald O'Malley", person_with_updates.given_name
+    assert_equal 'Quimby', person_with_updates.family_name
   end
 end
