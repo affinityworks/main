48b7199ed3eec872f21e8988288614d3c8d3cebf
Scott Willson <scott.willson@gmail.com>
2017-03-11 09:35:18 -0800

    Validate consistency of #email and #email_addresses

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,8 @@
+# Devise assumes each Person has a single `email` attribute/DB column in people table.
+# OSDI data model has multiple email_addresses.
 class Person < ApplicationRecord
   include Api::Identifiers
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -10,9 +10,9 @@ class Person < ApplicationRecord
   serialize :ethnicities, Array
   serialize :languages_spoken, Array
 
-  has_one :email_address
   has_one :employer_address, class_name: 'EmployerAddress'
 
+  has_many :email_addresses
   has_many :personal_addresses, class_name: 'PersonalAddress'
   has_many :phone_numbers
   has_many :profiles
@@ -25,15 +25,22 @@ class Person < ApplicationRecord
   has_many :groups, through: :memberships
 
   validate :name_or_email
-  #has_many :memberships
+  validate :email_matches_primary_email_address
 
-  def email_addresses
-    Array.wrap email_address
+  # Need to check against #email before save, so can't use `belongs_to`
+  def primary_email_address
+    email_addresses.detect(&:primary?)&.address
   end
 
   def name_or_email
     if given_name.blank? && family_name.blank? && email.blank?
       errors.add :email, ', given_name and family_name cannot be blank'
+    end
+  end
+
+  def email_matches_primary_email_address
+    if email.present? && primary_email_address.present? && email != primary_email_address
+      errors.add :email, "'#{email}' must match primary email_addresses '#{primary_email_address}'"
     end
   end
 
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  test "basic person associations" do
+  test 'basic person associations' do
     one = people(:one)
-    assert_kind_of( Person, one )
-    assert_kind_of EmailAddress, one.email_address
+    assert_kind_of Person, one
+    assert_kind_of EmailAddress, one.email_addresses.first
     assert_kind_of PhoneNumber, one.phone_numbers.first
     assert_kind_of Profile, one.profiles.first
     assert_kind_of Submission, one.submissions.first
@@ -12,17 +12,58 @@ class PersonTest < ActiveSupport::TestCase
     assert_kind_of Event, one.events.first
     assert_kind_of Address, one.personal_addresses.first
     assert_kind_of Address, one.employer_address
-
-    #todo add check for making sure polymorphism on adresses works.
   end
 
-  test "should not save person without name" do
+  test 'should not save person without name' do
     person = Person.new
     assert_not person.save
   end
 
-  test "membership in groups" do
+  test 'membership in groups' do
     one = people(:one)
     assert_kind_of Group, one.groups.first
+  end
+
+  test 'addresses' do
+    # Need to rename PersonalAddress to PostalAddress
+    assert_raise(NoMethodError) do
+      Person.create!(
+        employer_address: EmployerAddress.new,
+        personal_addresses: PersonalAddress.new
+      )
+    end
+  end
+
+  test 'email + email addresses' do
+    Person.create!(
+      email: 'lisa@example.com',
+      password: 'secret123',
+      email_addresses: [
+        EmailAddress.new(primary: true, address: 'lisa@example.com'),
+        EmailAddress.new(primary: false, address: 'ls127@aol.com')
+      ]
+    )
+  end
+
+  test 'primary email must match #email' do
+    person = Person.new(
+      email: 'ls127@example.com',
+      password: 'secret123',
+      email_addresses: [
+        EmailAddress.new(primary: true, address: 'lisa@example.com'),
+        EmailAddress.new(primary: false, address: 'ls127@aol.com')
+      ]
+    )
+    assert !person.valid?, 'person with mismtached email addresses should be invalid'
+  end
+
+  test '#email must be set to primary email address' do
+    person = Person.new(
+      email_addresses: [
+        EmailAddress.new(primary: true, address: 'lisa@example.com'),
+        EmailAddress.new(primary: false, address: 'ls127@aol.com')
+      ]
+    )
+    assert !person.valid?
   end
 end
