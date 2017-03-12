86bf194b7c58e1d13eebac7c29aea4f3447dc26c
Scott Willson <scott.willson@gmail.com>
2017-03-12 16:36:52 -0700

    #14 Email address should be unique per person and indexed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228031144) do
+ActiveRecord::Schema.define(version: 20170312232807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,6 +224,7 @@ ActiveRecord::Schema.define(version: 20170228031144) do
     t.integer  "person_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["address", "person_id"], name: "index_email_addresses_on_address_and_person_id", unique: true, using: :btree
     t.index ["person_id"], name: "index_email_addresses_on_person_id", using: :btree
   end
 
