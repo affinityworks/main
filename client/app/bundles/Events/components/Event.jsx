cc02eb61ea3de5df2bae450bc99ae68bd034684b
Scott Willson <scott.willson@gmail.com>
2017-03-20 08:04:58 -0700

    Expect, and try to provide, full line of Event info via JSON API

--- client/app/bundles/Events/components/Event.jsx
+++ client/app/bundles/Events/components/Event.jsx
@@ -10,12 +10,12 @@ export default class Event extends React.Component {
     return (
     <tr>
       <td><a href="" data-toggle="modal" data-target="#event_add-modal">{this.state.title}</a></td>
-      <td>group</td>
-      <td>formatDate date "%m/%d/%Y" start_time </td>
-      <td>status</td>
-      <td>stats.invited_count</td>
-      <td>stats.rsvp_count</td>
-      <td>stats.attended_count</td>
+      <td>{this.state.organizer.name}</td>
+      <td>{this.state.start_date}</td>
+      <td>{this.state.status}</td>
+      <td>{this.state.invited_count}</td>
+      <td>{this.state.rsvp_count}</td>
+      <td>{this.state.attended_count}</td>
       <td><a href="" className="event_list-toggle"> <i className="fa fa-users"></i> View Participants</a></td>
     </tr>
     );
