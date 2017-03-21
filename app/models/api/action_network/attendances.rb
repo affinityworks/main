01fa5ffe9d1918758cf725526d2b8edcf146c07f
Scott Willson <scott.willson@gmail.com>
2017-03-21 14:15:51 -0700

    Follow pagination when importing from Action Network (#22)
    * Add OSDI API Event representer
    
    * Import Events from Action Network (name and title only)
    
    * Fix minor lint
    
    * Put event and event API tests in proper files
    
    * #13 Import event attributes (but not related objects yet)
    
    * #13 Fix test to test Api::EventsRepresenter
    
    * #13 Rename Event#type to Event#osdi_type to fix conflict with Rails STI 'type' column convention
    
    * #13 Add indentiers to all OSDI resources as Postgres array type (doesn't work quite right)
    
    * #13 Correctly add identifiers
    
    * #13 Use : not - for identifiers
    
    * #13 Almost make identifiers work
    
    * #13 Fix last test failure
    
    * Improve test to run without DB
    
    * #13 Only import new Action Network events
    
    * #13 Import AN events in a transaction
    
    * Tone down Rubocop complexity limit
    
    * #13 Merge more-recent existing events on import
    
    * #13 Move API import code into Api::Events. Decompose import method.
    
    * Improve Rubocop linting
    
    * #13 Add Attendance identifiers
    
    * #13 Stub out incomplete Action Network Attendance import. Add and rename OSDI utility methods (need de-duplication).
    
    * Add basic Code Climate config to kick off Brakeman security checks. (And other automated checks.)
    
    * #13 Allow people with blank names or email (but not both)
    
    * #13 Import attendances from Action Network with placeholder people
    
    * Remove temp file junk
    
    * #13 Consolidate duplicate identifiers methods
    
    * #13 DRY-up representers
    
    * #13 Encapsulate scope for outdated existing copies of foreign system records
    
    * Replace TODO with comment
    
    * Replace events.events and acceptances.acceptances with action_network.events and action_network.acceptances
    
    * Remove TODOs captured elsewhere
    
    * #14 One more sweep to name and organize API and representer classes
    
    * Save WIP
    
    * WIP
    
    * Validate consistency of #email and #email_addresses
    
    * Map all EmailAddress properties properly
    
    * Add API README. Need a location to explain all the API classes.
    
    * Wrap Person relation in Api::Collections::People instance so representers can be used for import and export
    
    * Use Ruby 1.9 hash syntax
    
    * Use Api::Collections::Collection to represent OSDI API request and response payloads
    
    * Use Api::Collections::Collection to represent OSDI API request and response payloads
    
    * Drop conversion to ? methods so representers can work for import and export
    
    * Drop conversion to ? methods so representers can work for import and export
    
    * Improve email fixture realism
    
    * Add detail to representer tests
    
    * Provide explicit class for imported collection members
    
    * Test that previously-imported Action Network people are updated
    
    * Import Action Network Attendances
    
    * Minor formatting improvement of validation error message
    
    * Log import errors
    
    * Be more lenient about imported email addresses and sanitize (reject) invalid ones
    
    * Flesh out API README
    
    * Update external API versio to OSDI 1.1.2
    
    * Explain what all the different API classes do
    
    * #14 Graft pagination on to Action Network people import
    
    * #14 Graft pagination on to Action Network people import
    
    * #14 Email address should be unique per person and indexed
    
    * #14 Report overall import counts and use return arg, not instance var for next page URI
    
    * Make stateless classes nito modules
    
    * Extract logger
    
    * Extract Import#partition
    
    * Extract Import#request_resources_from_action_network
    
    * Consolidate duplicate import code
    
    * Fxi class name in log message
    
    * Fix overwritten API README

--- app/models/api/action_network/attendances.rb
+++ app/models/api/action_network/attendances.rb
@@ -1,53 +1,38 @@
-class Api::ActionNetwork::Attendances
-  def self.import!(event)
-    action_network_event_id = event.identifier_id('action_network')
-    logger.info "Api::ActionNetwork::Attendances#import! from https://actionnetwork.org/api/v2/events/#{action_network_event_id}/attendances"
+module Api::ActionNetwork::Attendances
+  extend Api::ActionNetwork::Import
 
-    attendances = request_attendances_from_action_network(action_network_event_id)
+  def self.resource
+    'attendance'
+  end
+
+  def self.representer_class
+    Api::ActionNetwork::AttendancesRepresenter
+  end
+
+  def self.import!(event)
+    existing_count = 0
+    new_count = 0
+    updated_count = 0
+    action_network_event_id = event.identifier_id('action_network')
+    next_uri = "https://actionnetwork.org/api/v2/events/#{action_network_event_id}/attendances"
+
+    logger.info "Api::ActionNetwork::Attendances#import! from #{next_uri}"
 
     Attendance.transaction do
-      existing_attendances, new_attendances = partition_attendances(attendances)
-      updated_count = update_attendances(existing_attendances)
-      logger.debug "Api::ActionNetwork::Attendances#import! new: #{new_attendances.size} existing: #{existing_attendances.size} updated: #{updated_count}"
-      new_attendances = associate_with_person(new_attendances, event.id)
-      new_attendances.each(&:save!)
+      while next_uri
+        attendances, next_uri = request_resources_from_action_network(next_uri)
+
+        existing_attendances, new_attendances = partition(attendances)
+
+        new_count += new_attendances.size
+        existing_count += existing_count.size
+        updated_count = update_resources(existing_attendances)
+
+        new_attendances = associate_with_person(new_attendances, event.id)
+        new_attendances.each(&:save!)
+      end
+      logger.debug "Api::ActionNetwork::Attendances#import! new: #{new_count} existing: #{existing_count} updated: #{updated_count}"
     end
-  end
-
-  def self.request_attendances_from_action_network(action_network_event_id)
-    collection = Api::Collections::Attendances.new
-    client = Api::ActionNetwork::AttendancesRepresenter.new(collection)
-    client.get(uri: "https://actionnetwork.org/api/v2/events/#{action_network_event_id}/attendances", as: 'application/json') do |request|
-      request['OSDI-API-TOKEN'] = Rails.application.secrets.action_network_api_token
-    end
-
-    logger.debug "Api::ActionNetwork::Attendances#import! attendances: #{collection.attendances.size}"
-    collection.attendances
-  end
-
-  def self.partition_attendances(attendances)
-    attendances.partition do |attendance|
-      action_network_identifier = attendance.identifier('action_network')
-      Attendance.any_identifier(action_network_identifier).exists?
-    end
-  end
-
-  # Update all attributes for attendances that already exist and have not been modified after import
-  # We may want to do something different
-  def self.update_attendances(existing_attendances)
-    updated_count = 0
-    existing_attendances.each do |attendance|
-      old_attendance = Attendance.outdated_existing(attendance, 'action_network').first
-
-      next unless old_attendance
-
-      updated_count += 1
-      attributes = attendance.attributes
-      attributes.delete_if { |_, v| v.nil? }
-      old_attendance.update_attributes! attributes
-    end
-
-    updated_count
   end
 
   def self.associate_with_person(new_attendances, event_id)
@@ -56,9 +41,5 @@ class Api::ActionNetwork::Attendances
       person = Person.any_identifier("action_network:#{attendance.person_uuid}").first!
       attendance.person_id = person.id
     end
-  end
-
-  def self.logger
-    Attendance.logger
   end
 end
