cef9f3afc53890691bf3d58f2357e8dba738d6f5
Matias Nieves <matinieves@gmail.com>
2017-04-27 02:14:53 -0300

    Add feature to filter events by title. (#60)

--- client/app/bundles/Events/components/Events.jsx
+++ client/app/bundles/Events/components/Events.jsx
@@ -1,18 +1,28 @@
 import axios from 'axios';
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
 
 import Event from './Event';
 import EventsFilter from './EventsFilter';
 
-export default class Events extends React.Component {
+export default class Events extends Component {
   constructor(props, _railsContext) {
     super(props);
 
     this.state = {events: []};
+    this.filterEvents = this.filterEvents.bind(this);
   }
 
-  componentDidMount () {
-    axios.get(`events.json`)
+  componentDidMount() {
+    this.getEvents(null);
+  }
+
+  filterEvents(searchTerm) {
+    this.getEvents(searchTerm);
+  }
+
+  getEvents(filter) {
+    const uri = filter ? `events.json?filter=${filter}` : `events.json`;
+    axios.get(uri)
       .then(res => {
         const events = res.data.data;
         this.setState({ events });
@@ -22,7 +32,7 @@ export default class Events extends React.Component {
   render() {
     return (
       <div>
-        <EventsFilter />
+        <EventsFilter onSearchSubmit={this.filterEvents}/>
         <br />
         <div className='list-group'>
           {this.state.events.map(event => <Event key={event.id} event={event} />)}
--- test/controllers/events_controllers_test.rb
+++ test/controllers/events_controllers_test.rb
@@ -15,4 +15,13 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     json = JSON.parse(@response.body)
     assert_equal 2, json['data'].size
   end
+
+  test 'get #index with filter' do
+    sign_in people(:one)
+    get events_url(filter: Event.first.title.first(4)), as: :json
+    assert_response :success
+    json = JSON.parse(@response.body)
+    assert_equal 1, json['data'].size
+    assert_equal Event.first.id, json['data'].first['id'].to_i
+  end
 end
