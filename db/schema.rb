f95a093dce80bcb9cdf9841673cfab825cd4f47b
rabble <evan@protest.net>
2017-05-08 14:54:20 -0700

    updated schema post migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504155342) do
+ActiveRecord::Schema.define(version: 20170503191123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -449,6 +449,7 @@ ActiveRecord::Schema.define(version: 20170504155342) do
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.text     "identifiers",            default: [],              array: true
+    t.string   "email"
     t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true, using: :btree
   end
 
