e86f83e46b34be411bef4361e0bf0cbae786a363
Matias Nieves <matinieves@gmail.com>
2017-05-11 11:57:01 -0300

    Redirect non organizer group members to group public page after logs in.

--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -10,7 +10,17 @@ class MembershipTest < ActiveSupport::TestCase
     assert_kind_of Membership, one.group.memberships.first
 
     assert_kind_of Group, one.person.groups.first
-    
+
     #assert_kind_of Person, one.group.members.first
+  end
+
+  test '#organizer' do
+    organizers = Membership.where(role: 'organizer')
+    assert_equal Membership.organizer, organizers
+  end
+
+  test '#member' do
+    members = Membership.where(role: 'member')
+    assert_equal Membership.member, members
   end
 end
