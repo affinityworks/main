9720955e094e250d1a84bc39651b552fab3c28d4
Agustin Cornu <agustinrhcp@gmail.com>
2017-05-11 21:10:11 -0300

    Fix controller tests.

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -3,19 +3,4 @@ class Api::V1::BaseApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :doorkeeper_authorize!
   # before_action :authenticate_api_user_from_token!
-
-  private
-
-  def authenticate_api_user_from_token!
-    osdi_api_token = request.headers['HTTP_OSDI_API_TOKEN'] || params[:osdi_api_token]
-
-    unless osdi_api_token.present?
-      return head :unauthorized
-    end
-
-    api_user = Api::User.first_by_osdi_api_token(osdi_api_token)
-    if api_user
-      sign_in api_user, store: false
-    end
-  end
 end
