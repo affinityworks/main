31b4390ec100b9031304f2f96effa67991a79bed
rabble <evan@protest.net>
2017-05-15 14:48:05 -0700

    changing api key to use the test one

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-group = Group.create(name: 'Test Group', an_api_key: 'c96dc7a808ed80fca8bb4953f8ac10bf')
+group = Group.create(name: 'Test Group', an_api_key: '7cc9eff8d105f3ff34c6bf7683abbec6')
 admin = Person.create(family_name: 'Admin', given_name: 'Test', password: 'password', admin: true)
 admin.email_addresses(EmailAddress.create(address: 'test@admin.com'))
 member = Person.create(family_name: 'Member', given_name: 'Test', password: 'password')
