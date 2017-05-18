7870697252f0fac64636478112e7c8cf41211798
rabble <evan@protest.net>
2017-05-18 09:45:05 -0700

    basic profile page, still ugly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512183837) do
+ActiveRecord::Schema.define(version: 20170516052527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -488,6 +488,8 @@ ActiveRecord::Schema.define(version: 20170512183837) do
     t.inet     "last_sign_in_ip"
     t.text     "identifiers",            default: [],                 array: true
     t.boolean  "admin",                  default: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true, using: :btree
   end
 
