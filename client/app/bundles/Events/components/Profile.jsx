e5734a8a736f77edce089c3b69c9b2406492e3b3
Matias Nieves <matinieves@gmail.com>
2017-05-19 16:29:49 -0300

    Show Connect with Facebook button in profile page.

--- client/app/bundles/Events/components/Profile.jsx
+++ client/app/bundles/Events/components/Profile.jsx
@@ -7,15 +7,38 @@ class Profile extends Component {
     super(props);
   }
 
+  linkWithFacebook() {
+    if (!this.props.currentUser.linked_with_facebook)
+      return (
+        <a href='/admin/auth/facebook' className='btn btn-primary'>
+          Connect with Facebook
+        </a>
+      )
+    else
+      return (
+        <a href='#' className='btn btn-danger'>
+          Disconnect from Facebook
+        </a>
+      )
+  }
+
   render() {
-    //const { currentUser, currentGroup } = this.state;
+    const { currentUser, currentGroup } = this.props;
     return (
-      <div className='list-group-item'>
-        <h1>
-          Welcome {this.props.currentUser.given_name} {this.props.currentUser.family_name}
-        </h1>
-        <h3>Your Groups</h3>
-        <Groups location={this.props.location} />
+      <div>
+        <div className='row'>
+          <div className='col-md-6'>
+            <h1>Welcome {currentUser.given_name} {currentUser.family_name}</h1>
+          </div>
+          <div className='col-md-3 offset-md-3 text-right'>
+            { this.linkWithFacebook(currentUser) }
+          </div>
+        </div>
+        <br/>
+        <div className='list-group-item'>
+          <h3>Your Groups</h3>
+          <Groups location={this.props.location} />
+        </div>
       </div>
     )
   }
