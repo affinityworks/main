f8facd38347d32f35b1c91dbe9d4b9921c155cac
Matias Nieves <matinieves@gmail.com>
2017-05-19 14:57:29 -0300

    Add OmniauthCallbacks Controller.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,8 +251,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
 
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
-
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
+                  callback_url: ENV["CALLBACK_URL"] #OAuth v2.0.1 callback url issue
 
 
   # ==> Warden configuration
