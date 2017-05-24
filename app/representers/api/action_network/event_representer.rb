1ccb3638040662898c9284f0c7ff84595e9fd56f
Agustín Cornú <agustincornu@fastmail.com>
2017-05-24 16:44:34 -0300

    Add creator in when importing events. (#199)

--- app/representers/api/action_network/event_representer.rb
+++ app/representers/api/action_network/event_representer.rb
@@ -1,3 +1,4 @@
 class Api::ActionNetwork::EventRepresenter < Api::Resources::EventRepresenter
   property :organizer, as: 'osdi:organizer', decorator: Api::Resources::PersonRepresenter, class: Person
+  property :creator, as: 'osdi:creator', decorator: Api::Resources::PersonRepresenter, class: Person
 end
