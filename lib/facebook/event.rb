98521fae232d6f8ed23528f1c78e6704505b2e3f
Matias Nieves <matinieves@gmail.com>
2017-05-25 14:38:22 -0300

    Enable usage of Facebook urls or Facebook event id in Facebook wrapper.

--- lib/facebook/event.rb
+++ lib/facebook/event.rb
@@ -1,4 +1,7 @@
 class Facebook::Event
+  FACEBOOK_DOMAIN = 'facebook.com'
+  EVENT_ID_REGEXP = /facebook\.com\/events\/(?<event_id>\w+)/
+
   def initialize(identity)
     @identity = identity
     @options = { access_token: @identity.access_token }
@@ -9,7 +12,19 @@ class Facebook::Event
     @api_agent.get("/#{@identity.uid}/events", @options)
   end
 
-  def find(event_id)
-    @api_agent.get("/#{event_id}", @options)
+  def find(event_data)
+    @api_agent.get("/#{get_event_id(event_data)}", @options)
+  rescue
+    nil
+  end
+
+  private
+
+  def get_event_id(event_data)
+    if event_data.include?(FACEBOOK_DOMAIN)
+      event_data.match(EVENT_ID_REGEXP)['event_id']
+    else
+      event_data
+    end
   end
 end
