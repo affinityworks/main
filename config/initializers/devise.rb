20a54f840e936460f557b7b8aff4b273bd86ceb0
rabble <evan@protest.net>
2017-05-31 17:11:43 -0700

    tweaking facebook connect

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
 
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
+  config.omniauth :facebook, ENV.fetch("FACEBOOK_APP_ID"), ENV.fetch("FACEBOOK_APP_SECRET"),
                   scope: 'user_events,rsvp_event'
 
 
