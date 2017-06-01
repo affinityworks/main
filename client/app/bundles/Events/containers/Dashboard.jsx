fa10dc671e981c6dec3de4cc89c6a1e0f5dbd657
Agustin Cornu <agustinrhcp@gmail.com>
2017-06-01 15:48:06 -0300

    Add groups to nav.

---
+++ client/app/bundles/Events/containers/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Nav from '../components/Nav';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
