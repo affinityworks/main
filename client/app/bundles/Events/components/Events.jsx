0b10f811b57eda3a63acd795f75b2833f389f43a
Agustín Cornú <agustincornu@fastmail.com>
2017-06-06 16:38:24 -0300

    Breadcrumbs for affiliates (#274)
    * Link to groups dashboard if national organizer.
    
    * Create affiliate group in seeds.
    
    * Add breadcrumbs for affiliate groups and update header.

--- client/app/bundles/Events/components/Events.jsx
+++ client/app/bundles/Events/components/Events.jsx
@@ -80,7 +80,7 @@ class Events extends Component {
       <div>
         <div className='row'>
 
-          <div className='col-5'>
+          <div className='col-6'>
             <EventsFilter onSearchSubmit={this.filterEvents} filter={filter} />
           </div>
           <div className='col-3 offset-4 text-right'>
