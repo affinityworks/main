9c431d031b864aab22f337d288f40f1d7754a23c
Matias Nieves <matinieves@gmail.com>
2017-06-07 15:21:57 -0300

    Add Group permitted parameters for Admin section. (#278)

--- app/admin/group.rb
+++ app/admin/group.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register Group do
 
+  permit_params :name, :description, :creator, :origin_system, :summary, :browser_url, :featured_image_url,
+    :modified_by, :an_api_key, :synced_at, :address
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
