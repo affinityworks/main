eb9f2096301d23e666f0ebfe53432b46343936ef
Matias Nieves <matinieves@gmail.com>
2017-06-07 01:28:08 -0300

    Allow to send email to several members. (#275)

--- client/app/bundles/Events/components/Member.jsx
+++ client/app/bundles/Events/components/Member.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import { membersPath } from '../utils/Pathnames';
 
-const Member = (props) => {
-  const { member } = props;
-  if(!member) { return null }
-
-  const emailAddressLink = () => {
-    const email = member['primary-email-address']
+class Member extends Component {
+  emailAddressLink() {
+    const email = this.props.member['primary-email-address']
     if (email)
-      return <a href={`mailto:'${email}'`} className='fa fa-envelope-o'/>
+      return <a href={`mailto:${email}`} className='fa fa-envelope-o'/>
   }
 
-  const localityAndRegion = () => {
-    const address = member['primary-personal-address']
+  localityAndRegion() {
+    const address = this.props.member['primary-personal-address']
     if (address) {
       if (address['locality'] && address['region']) {
         return <span>{address['locality']}, {address['region']}</span>
@@ -32,19 +29,41 @@ const Member = (props) => {
     return <span></span>
   }
 
-  return(
-    <tr>
-      <td>
-        <Link to={`${membersPath()}/${props.id}`}>
-          {member['given-name']} {member['family-name']}
-        </Link>
-      </td>
-      <td>{member['primary-phone-number']}</td>
-      <td>{ localityAndRegion() }</td>
-      <td>{props.role}</td>
-      <td>{ emailAddressLink() }</td>
-    </tr>
-  )
+  handleChange(ev) {
+    const { member, onCheckboxChecked, onCheckboxUnChecked } = this.props;
+    const email = member['primary-email-address'];
+
+    if(ev.target.checked && email)
+      onCheckboxChecked(email);
+    else
+      onCheckboxUnChecked(email)
+  }
+
+  showEmailCheckbox() {
+    const email = this.props.member['primary-email-address'];
+    return <input type='checkbox' disabled={email ? false : true} onChange={this.handleChange.bind(this)}/>
+  }
+
+  render() {
+    const { member, role, id } = this.props;
+
+    if(!member) { return null }
+
+    return(
+      <tr>
+        <td>{this.showEmailCheckbox()}</td>
+        <td>
+          <Link to={`${membersPath()}/${id}`}>
+            {member['given-name']} {member['family-name']}
+          </Link>
+        </td>
+        <td>{member['primary-phone-number']}</td>
+        <td>{this.localityAndRegion()}</td>
+        <td>{role}</td>
+        <td>{this.emailAddressLink()}</td>
+      </tr>
+    );
+  }
 }
 
 export default Member;
