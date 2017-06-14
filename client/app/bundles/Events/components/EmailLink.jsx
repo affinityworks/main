a6af3ceddd5e5e9e5d3095e0a8fd6227ea825bf9
Agustín Cornú <agustincornu@fastmail.com>
2017-06-14 15:11:29 -0300

    Return null in EmailLink if not email is given. (#306)

--- client/app/bundles/Events/components/EmailLink.jsx
+++ client/app/bundles/Events/components/EmailLink.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const EmailLink = ({ email, style }) => {
   if (email)
     return <a href={`mailto:${email}`} style={style} className='fa fa-envelope-o'/>
+
+  return null;
 }
 
 export default EmailLink;
