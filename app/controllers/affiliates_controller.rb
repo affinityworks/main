1bd8cf05074cd8cae58a528cd5b3d3ce5b1e27c7
Agustin Cornu <agustinrhcp@gmail.com>
2017-06-16 15:29:54 -0300

    Add affiliates index and refactor Groups component.

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -1,3 +1,18 @@
 class AffiliatesController < ApplicationController
   before_action :authenticate_person!
+
+  def index
+    affiliates = current_person.groups.find(params[:group_id]).affiliates.page(params[:page])
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          affiliates: JsonApi::GroupsRepresenter.for_collection.new(affiliates),
+          total_pages: affiliates.total_pages,
+          page: affiliates.current_page
+        }.to_json
+      end
+    end
+  end
 end
