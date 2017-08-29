26d6f72437d7915affac50ae7e4314105f5d8e13
rabble <evan@protest.net>
2017-08-29 11:17:21 -0700

    fixing jobs so it reqeusts a list of people not persons

--- app/jobs/action_network_request_person_job.rb
+++ app/jobs/action_network_request_person_job.rb
@@ -5,7 +5,7 @@ class ActionNetworkRequestPersonJob < ApplicationJob
   def perform(next_uri,group)
     puts "in perform #{next_uri}"
     logs = []
-    people, next_uri = Api::ActionNetwork::Person.request_resources_from_action_network(next_uri, group)
+    people, next_uri = Api::ActionNetwork::People.request_resources_from_action_network(next_uri, group)
     people.each(&:sanitize_email_addresses)
     people.each do |new_person|
       logs << Api::ActionNetwork::Person.after_import(new_person, group)
