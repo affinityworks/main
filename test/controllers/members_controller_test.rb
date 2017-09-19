9fbad2893efa58f53c03043a4da603358a49e65f
Scott Willson <scott.willson@gmail.com>
2017-09-19 16:26:37 -0700

    Normalize whitespace

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -16,7 +16,7 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     get group_members_url(group_id: group.id), as: :json
     assert_response :success
     json = JSON.parse(response.body)
-     
+
     member_count = group.members.count
     group.affiliates.each {|a| member_count += a.members.count}
     assert_equal member_count, json['members']['data'].count
@@ -25,7 +25,7 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     assert_includes response_members_ids, affiliate_member.id
   end
 
-# having issues with posting params for this 
+# having issues with posting params for this
 #No route matches {:action=>"new", :controller=>"members"} missing required keys: [:group_id]
 
   test 'create member' do
@@ -34,26 +34,26 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     sign_in person
     get new_group_member_url(:group_id => group_id)
     assert_response :success
-     
+
 
     assert_difference -> { Person.count } do
-      post group_members_path(group_id), params:  { 
-        person: { 
-          given_name: "Test", 
-          family_name: "TestFamily", 
-          party_identification: "", 
-          birthdate: "", 
-          employer: ""}, 
-        email_address: {email_address: "yes@email.com"}, 
+      post group_members_path(group_id), params:  {
+        person: {
+          given_name: "Test",
+          family_name: "TestFamily",
+          party_identification: "",
+          birthdate: "",
+          employer: ""},
+        email_address: {email_address: "yes@email.com"},
         phone_number: {phone_number: "555-555-5555"}, group_id: group_id }  , as: :json
     end
 
-    
+
     assert_response :success
     assert EmailAddress.find_by_address("yes@email.com")
     assert EmailAddress.find_by_address("yes@email.com").person.groups.include? groups(:test)
     assert groups(:test).members.include? EmailAddress.find_by_address("yes@email.com").person
-  end 
+  end
 
 
   test 'create member with real form data' do
@@ -72,7 +72,7 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     assert EmailAddress.find_by_address("asdf@sadf.com").person.groups.include? groups(:test)
     assert groups(:test).members.include? EmailAddress.find_by_address("asdf@sadf.com").person
 
-  end 
+  end
 
 
   test "affiliated organizer can view member" do
@@ -83,9 +83,9 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     sign_in organizer
 
     get group_member_url(group_id: group.id, id: member.id), as: :json
-    assert_response :success   
+    assert_response :success
   end
-  
+
   test "affiliated organizer can directly view member" do
     organizer = people(:organizer)
     group = groups(:fourth)
@@ -94,7 +94,7 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     sign_in organizer
 
     get member_url(id: member.id), as: :json
-    assert_response :success   
+    assert_response :success
   end
 
   test "non-affiliatd organizer cant view member" do
@@ -107,7 +107,7 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     get group_member_url(group_id: group.id, id: member.id), as: :json
     assert_response 403
   end
-  
+
   test "member cant view other members of a group" do
     organizer = people(:member2)
     group = groups(:fourth)
@@ -118,7 +118,7 @@ class MembersControllerTest < ActionDispatch::IntegrationTest
     get group_member_url(group_id: group.id, id: member.id), as: :json
     assert_response 403
   end
- 
+
   test 'members shouldnt be able to see list' do
     person = people(:member1)
     sign_in person
