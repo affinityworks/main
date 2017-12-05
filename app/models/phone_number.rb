649a691c46efb6aede30929246747157d4ec8835
Juan Mejia <juan.mejia@koombea.com>
2017-12-05 13:56:24 -0500

    [issue 403] Fix phone number validation (#428)

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -2,7 +2,7 @@ class PhoneNumber < ApplicationRecord
   include ArelHelpers::ArelTable
   belongs_to :person
 
-  validates :number, :presence => true, :numericality => true
+  validates :number, :presence => true, format: { with:  /\A(\((\d){3}\)\s|(\+1-)?(\d){3}-)(\d){3}-(\d){4}|(\d){10}\z/, message: 'Only numbers format are allowed' }
   validates_uniqueness_of :number, scope: :person_id
 
   scope :primary, -> { where(primary: true) }
