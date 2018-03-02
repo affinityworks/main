41eb9279e675d2e3a341067c9b948b9bb0891c78
aguestuser <aguestuser@riseup.net>
2018-03-01 21:41:42 -0500

    [#512|fix] fix organizer mailer impl & tests

--- app/mailers/organizer_mailer.rb
+++ app/mailers/organizer_mailer.rb
@@ -1,9 +1,9 @@
 class OrganizerMailer < ApplicationMailer
 
   def new_subgroup_email(organizer, subgroup, form)
-    @organizer = organizer; @subgroup = subgroup, @form = form
-    mail(to: @user.primary_email_address,
-         subject: "Welcome to #{@subgroup} name!")
+    @organizer = organizer; @subgroup = subgroup; @form = form
+    mail(to: @organizer.primary_email_address,
+         subject: "Welcome to #{@subgroup.name}")
 
   end
 end
--- test/mailers/organizer_mailer_test.rb
+++ test/mailers/organizer_mailer_test.rb
@@ -1,8 +1,10 @@
+require_relative "../test_helper"
+
 class OrganizerMailerTest < ActionMailer::TestCase
 
   describe "new_subgroup_email" do
     let(:organizer){ people(:organizer) }
-    let(:subgroup){ groups(:one) }
+    let(:subgroup){ groups(:subgroup) }
     let(:signup_form){ forms(:group_signup) }
     let(:mail){ OrganizerMailer.new_subgroup_email(organizer, subgroup, signup_form) }
     let(:body){ mail.body.raw_source }
@@ -12,19 +14,19 @@ class OrganizerMailerTest < ActionMailer::TestCase
     end
 
     it "is addressed to organizer" do
-      mail.to.must_equal organizer.primary_email_address
+      mail.to.must_equal [organizer.primary_email_address]
     end
 
     it "links to signup form" do
-      body.must.match "/groups/#{group.id}/signup_forms/#{signup_form.id}/new"
+      body.must_match "/groups/#{subgroup.id}/signup_forms/#{signup_form.id}/signups/new"
     end
 
     it "links to organizer profile page" do
-      body.must.match "/profile"
+      body.must_match "/profile"
     end
 
     it "provides help email" do
-      body.must.match "help@affinity.works"
+      body.must_match "help@affinity.works"
     end
   end
 end
