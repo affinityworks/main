e85ec6a6cc4c37658e8f898dc14ad2283683f5b3
aguestuser <aguestuser@riseup.net>
2018-03-15 09:59:47 -0400

    [#515] ding dong the activity feed is dead!

--- client/app/bundles/Events/containers/Dashboard.jsx
+++ client/app/bundles/Events/containers/Dashboard.jsx
@@ -19,39 +19,7 @@ class Dashboard extends Component {
 
   componentWillMount() {
     const { groupId } = this.props.match.params;
-
     this.props.fetchGroup(groupId);
-
-    client.get(`${dashboardPath()}.json`).then(response => {
-      this.setState({ ...response.data })
-    }).catch(alert => {
-      this.props.addAlert(alert);
-    });
-  }
-
-  showAttendancesActivity() {
-    const { attendances } = this.state;
-
-    if (!attendances.length)
-      return null
-
-    return (
-      <div>
-        <br />
-        <h3>New Recorded Attendances</h3>
-        <AttendanceActivityFeed attendances={attendances} />
-      </div>
-    )
-  }
-
-  hasActivity() {
-    const { people, events, attendances, sync } = this.state;
-    return !! people.updated.length
-      || !! people.created.length
-      || !! events.created.length
-      || !! events.updated.length
-      || !! attendances.length
-      || !! (sync && sync.data)
   }
 
   renderTextEditor () {
@@ -75,44 +43,23 @@ class Dashboard extends Component {
 
     return (
       <div>
-        <Breadcrumbs active='Dashboard' location={this.props.location} />
 
+        <Breadcrumbs active='Dashboard' location={this.props.location} />
         <br />
 
         <Nav activeTab='dashboard' />
-
         <br />
 
         {this.renderTextEditor()}
-
         <hr/>
-        {this.hasActivity() && <h2>Activity Feed</h2> || <h4> There's no recent activity for this group.</h4>}
-        {this.hasActivity() && <hr />}
-
-        { !!sync && !!sync.data && <SyncActivityFeed sync={sync} />}
 
         <div className="edit-button">
-        <UserAuth allowed={['organizer']}>
-          <a href="edit">
+          <UserAuth allowed={['organizer']}>
+            <a href="edit">
               <button className='btn btn-primary'>Edit Group</button>
             </a>
-        </UserAuth>
+          </UserAuth>
           <br />
-        </div>
-
-        {(!!events.updated.length || !!events.created.length) && <h3>Events</h3>}
-        <div className='list-group'>
-          <EventActivityFeed events={events.updated} type='Updated'/>
-          <EventActivityFeed events={events.created} type='Created'/>
-        </div>
-
-        {this.showAttendancesActivity()}
-
-        <br />
-        {(!!people.updated.length || !!people.created.length) && <h3>People</h3>}
-        <div className='list-group'>
-          <PersonActivityFeed people={people.updated} type='Updated'/>
-          <PersonActivityFeed people={people.created} type='Created'/>
         </div>
       </div>
     );
