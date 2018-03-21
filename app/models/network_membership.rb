33e6c6913d2253dd57ea8f7c4722d0424592dec3
aguestuser <aguestuser@riseup.net>
2018-03-20 22:25:43 -0400

    [#523|perf] index network_memberships
    * joins (for faster lookup)
    * primary (for faster only-one-primary validation)

--- app/models/network_membership.rb
+++ app/models/network_membership.rb
@@ -7,13 +7,11 @@ class NetworkMembership < ApplicationRecord
   scope :primary, ->{ where(primary: true) }
 
   # VALIDATIONS
-  # TODO: index group_id:newtork_id (compound) if this is ever slow
   validates :group_id, uniqueness: {
               scope: :network_id,
               message: 'only one membership per network permitted'
             }
 
-  # TODO: index `primary`` if this is ever slow
   validates :primary, uniqueness: {
               scope: :group_id,
               message: 'only one primary network per group permitted'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320182830) do
+ActiveRecord::Schema.define(version: 20180321021928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -433,8 +433,11 @@ ActiveRecord::Schema.define(version: 20180320182830) do
     t.boolean  "primary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["group_id", "network_id"], name: "index_network_memberships_on_group_id_and_network_id", unique: true, using: :btree
     t.index ["group_id"], name: "index_network_memberships_on_group_id", using: :btree
+    t.index ["network_id", "group_id"], name: "index_network_memberships_on_network_id_and_group_id", unique: true, using: :btree
     t.index ["network_id"], name: "index_network_memberships_on_network_id", using: :btree
+    t.index ["primary"], name: "index_network_memberships_on_primary", using: :btree
   end
 
   create_table "networks", force: :cascade do |t|
