9843c0700fb04ce482e4489f862cc45c0dcc9018
James Vaughan <vaughanj10@gmail.com>
2018-03-25 15:14:42 -0400

    [#529] Updated test for Subgroups::Create service to not call google services if auth fails.

--- test/services/subgroups/create_test.rb
+++ test/services/subgroups/create_test.rb
@@ -15,11 +15,24 @@ class Subgroups::CreateTest < ActiveSupport::TestCase
         allow(FeatureToggle).to receive(:on?).and_return(true)
       end
 
-      it "calls GoogleAPI services" do
-        expect(GoogleAPI::GetAuthorization).to receive(:call)
+      it "calls GoogleAPI services if authorization is successful" do
+        auth = double("auth")
+        auth_service = instance_double(GoogleAPI::GetAuthorization, success?: true, result: auth)
+        expect(GoogleAPI::GetAuthorization).to receive(:call).and_return(auth_service)
         expect(GoogleAPI::CreateGoogleGroup).to receive(:call)
         expect(GoogleAPI::UpdateGoogleGroupSettings).to receive(:call)
         expect(GoogleAPI::AddMemberToGoogleGroup).to receive(:call)
+
+        Subgroups::Create.call(organizer: Person.new, subgroup: Group.new(name: "Test Group"))
+      end
+
+      it "does not call GoogleAPI services if authorization is not successful" do
+        auth = double("auth")
+        auth_service = instance_double(GoogleAPI::GetAuthorization, success?: false, result: auth)
+        allow(GoogleAPI::GetAuthorization).to receive(:call).and_return(auth_service)
+        expect(GoogleAPI::CreateGoogleGroup).to_not receive(:call)
+        expect(GoogleAPI::UpdateGoogleGroupSettings).to_not receive(:call)
+        expect(GoogleAPI::AddMemberToGoogleGroup).to_not receive(:call)
 
         Subgroups::Create.call(organizer: Person.new, subgroup: Group.new(name: "Test Group"))
       end
