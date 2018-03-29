da8759f06b9421e88cffddebd6a3993540b27169
aguestuser <aguestuser@riseup.net>
2018-03-28 21:29:29 -0400

    [#563] add class method to import gsuite key safely
    given the path to a gsuite credentials json file:
    
    * ensure filename is of the form `network_name_google_gsuite.json`,
      and that `network_name` is the valid snakecased name of a network
      that already exists in the app -- raise usage error otherwise
    * create a credentials directory for the network if it doesn't already exist
      (where the name of the dir is given as the md5 hash of the
      snakecased network name)
    * copy the credentials json into the credentials directory
    * retrieve credentials w/ convenience method `Network#google_gsuite_key_path`
    
    additionally, this commit:
    
    * edits and re-encrypts `network.yml`

--- app/models/network.rb
+++ app/models/network.rb
@@ -16,10 +16,43 @@ class Network < ApplicationRecord
   # NESTED ATTRIBUTES
   accepts_nested_attributes_for :members
 
+  # CLASS METHODS
+  class << self
+
+    def find_by_snakecase_name(snakecase_name)
+      find_by_name(snakecase_name.titleize)
+    end
+
+    def import_gsuite_key(path)
+      usage = "json filname must be formated as follows: " +
+              "<valid_network_name_in_snake_case>.google_gsuite_key.json"
+
+      key_regex = /([a-z_]*)_google_gsuite_key\.json/
+      name = path.match(key_regex)&.send(:[], 1)
+      network = Network.find_by_snake_case_name(name)
+      raise usage unless network
+
+      FileUtils.mkdir_p network.credentials_dir
+      FileUtils.cp path, network.google_gsuite_key_path
+
+      puts "Import complete. Delete #{path}" unless ENV['RAILS_ENV'] == 'test'
+    end
+  end
+
   # ACCESSORS
 
-  def google_gsuite_key
-    Rails.configuration.networks[snakecase_name]["google_gsuite_key"]
+  def name_as_md5_hash
+    Digest::MD5.hexdigest snakecase_name
+  end
+
+  def credentials_dir
+    # - credentials are stored as json inside of folders whose names
+    #   are given as the md5 hash of the network's snakecased name
+    "lib/network_credentials/#{name_as_md5_hash}"
+  end
+
+  def google_gsuite_key_path
+    "#{credentials_dir}/google_gsuite_key.json"
   end
 
   def google_gsuite_admin_email
@@ -30,6 +63,7 @@ class Network < ApplicationRecord
     Rails.configuration.networks[snakecase_name]["google_gsuite_email_base"]
   end
 
+  # TODO: rename this `name_as_snakecase`
   def snakecase_name
     name.downcase.split.join("_")
   end
