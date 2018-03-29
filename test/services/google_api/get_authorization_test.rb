0d805ed391c697ddd6bbbc5a24a134b0a3e90339
aguestuser <aguestuser@riseup.net>
2018-03-29 16:59:59 -0400

    [#563] import demo gsuite credentials, use for gsuite auth tests
    * securely import affinity gsuite credentials
    * associate them with "National Network" demo group/network
    * substitute national network in `google_api/get_authorization_test`
      to get tests green again
    
    **registered in blackbox:**
    * `lib/network_credentials/7af65312214cfedd28371a29af1c6f22/google_gsuite_key.json.gpg`
      (gsuite creds for national network)

--- test/services/google_api/get_authorization_test.rb
+++ test/services/google_api/get_authorization_test.rb
@@ -3,34 +3,47 @@ require_relative "../../test_helper"
 class GoogleAPI::GetAuthorizationTest < ActiveSupport::TestCase
   describe ".call" do
 
+    # TODO: (aguestuser|29 Mar 2018)
+    # - these tests make un-mocked/stubbed calls to the google api
+    #   (via `Google::Auth::ServiceAccountCredentials.make_creds` at `get_authorization.rb:36`)
+    # - as such, they must look for and find a valid credentials file
+    #   on their local file system and make a network call in order to pass
+    # - currenly we satisfy that criteria by allowthing them to look for
+    #   `lib/network_credentials/7af65312214cfedd28371a29af1c6f22/google_gsuite_key.json`
+    #   (which is always encrypted and gitignored, and thus safe)
+    # - however, that means these tests can never pass on travis unless we
+    #   encrypte them to a private key that we also store on travis, or expose
+    #   our gsuite credentials, both of which seem undesirable
+    # - a better strategy might be to
+    #   (1) provide a dummy plaintext credentials file (for "Foo Network")
+    #   (2) stub the call to `Google::Auth::ServiceAccountCredentials.make_creds`
+    # - leaving as a TODO for now, pending further discussion w/ @vaughan10
+
     before do
-      network = Network.new(name: "National Network")
+      network = Network.create(name: "Foo Network")
+      #network = networks(:national_network)
       @auth_service = GoogleAPI::GetAuthorization.new(network: network)
     end
 
     it "calls build_directory_service" do
-      skip
       expect(@auth_service).to receive(:get_authorization)
 
       @auth_service.call
     end
 
     it "returns correct authorizer sub" do
-      skip
       @auth_service.call
 
       @auth_service.result.sub.must_equal "james@affinity.works"
     end
 
     it "returns correct authorizer scope" do
-      skip
       @auth_service.call
 
       @auth_service.result.scope.must_equal GoogleAPI::GetAuthorization::SCOPES
     end
 
     it "returns with success status if Network has credentials file" do
-      skip
       @auth_service.call
 
       @auth_service.success?.must_equal true
