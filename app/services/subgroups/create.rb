efb9dd0267f971c15bf83c784e384aa9d34c10e5
aguestuser <aguestuser@riseup.net>
2018-03-30 21:51:14 -0400

    [#563] restore async email delivery, suggest same for api calls

--- app/services/subgroups/create.rb
+++ app/services/subgroups/create.rb
@@ -3,11 +3,16 @@ class Subgroups::Create
     def call(organizer:, subgroup:)
       OrganizerMailer
         .new_subgroup_email(organizer, subgroup, SignupForm.for(subgroup))
-        .deliver_now
+        .deliver_later
 
       if FeatureToggle.on?(:google_groups, subgroup)
+        # TODO (aguestuser|30 Mar 2018)
+        # - these api calls should probably happen in a job
+        # - they cause form to hang for several seconds before submit success
+        # - also: consider creating a wrapper class to encapsulate state
+        #   and replace below calls with 0-argument chained method calls
         service = GoogleAPI::GetAuthorization.call(network: subgroup.primary_network) 
-        
+
         return unless service.success?
 
         authorization = service.result
@@ -22,7 +27,7 @@ class Subgroups::Create
           authorization: authorization,
           google_group: google_group
         )
-        
+
         GoogleAPI::AddMemberToGoogleGroup.call(
           authorization: authorization,
           google_group: google_group,
