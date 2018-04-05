2a0daebb989bfe972d22934e6896debdf460c817
aguestuser <aguestuser@riseup.net>
2018-04-05 13:18:25 -0400

    [#554] show empty resource list if group has no links

--- client/app/bundles/Events/components/GroupResources.jsx
+++ client/app/bundles/Events/components/GroupResources.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-const GroupResources = ({signupUrl}) => (
+const GroupResources = ({resources}) => (
   <div>
     <h4>Resources:</h4>
-    <ul>
-      <li>
-        <a href={signupUrl}>Signup Page</a>
-      </li>
-    </ul>
+    {
+      EmptyListOf(resources) ||
+      <ul>
+        {resources.map(GroupResource)}
+      </ul>
+    }
   </div>
+);
+
+// return placeholder if all resources have no links
+const EmptyListOf = (resources) => (
+  resources.every(r => !Boolean(r.link)) ?
+    <div style={{color: 'lightgray'}}>
+      This group has no resources
+    </div> :
+    null
+);
+
+const GroupResource = (r) => (
+  r.link && <li>
+    {r.description}: <a href={r.link}>{r.link}</a>
+  </li>
 );
 
 export default GroupResources;
