3a5ba8fa263706f5ab7b32a959d5fc7d96ea45b7
James Vaughan <vaughanj10@gmail.com>
2018-05-31 19:13:44 -0400

    [#466] Added primary true to personal address and phone number factories.

--- test/factories/personal_address.rb
+++ test/factories/personal_address.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :personal_address do
     postal_code Faker::Address.zip_code
+    primary true
   end
 end
--- test/factories/phone_number.rb
+++ test/factories/phone_number.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :phone_number do
     number { 10.times.map { rand(1..9).to_s }.join }
+    primary true
   end
 end
